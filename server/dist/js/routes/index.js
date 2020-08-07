68946d9699907875b562ae45f5b29e949c3fadc7
Jamie McNeil <42184621+mcneiljv@users.noreply.github.com>
2020-08-07 14:52:58 -0400

    Fix client side app not being able to get the delete path.

--- server/dist/js/routes/index.js
+++ server/dist/js/routes/index.js
@@ -6,5 +6,5 @@ const router = express_1.Router();
 router.get("/todos", todos_1.getTodos);
 router.post("/add-todo", todos_1.addTodo);
 router.put("/edit-todo/:id", todos_1.updateTodo);
-router.delete("delete-todo/:id", todos_1.deleteTodo);
+router.delete("/delete-todo/:id", todos_1.deleteTodo);
 exports.default = router;
